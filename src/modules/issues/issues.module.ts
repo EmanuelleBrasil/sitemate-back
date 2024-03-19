5d7ba0297ab1bef556d579054eb9bd35733bc809
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2024-03-19 17:45:58 -0300

    create issues module

--- src/modules/issues/issues.module.ts
+++ src/modules/issues/issues.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { IssuesController } from './issues.controller';
+import { IssuesService } from './issues.service';
 
-@Module({})
+@Module({
+  controllers: [IssuesController],
+  providers: [IssuesService],
+})
 export class IssuesModule {}
