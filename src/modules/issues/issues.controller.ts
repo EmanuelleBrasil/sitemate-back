8b215cf7240107c711c2bf7e53bc22b48a267a74
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2024-03-19 18:13:36 -0300

    create issue controller

--- src/modules/issues/issues.controller.ts
+++ src/modules/issues/issues.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { IssuesService } from './issues.service';
+import { CreateIssueDto } from './dto/issue.dto';
+import { Issue } from './issue.model';
 
 @Controller('issues')
-export class IssuesController {}
+export class IssuesController {
+  constructor(private readonly issuesService: IssuesService) {}
+
+  @Post()
+  async createIssue(@Body() issue: CreateIssueDto): Promise<Issue> {
+    return this.issuesService.createIssue(issue);
+  }
+}
